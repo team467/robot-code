b111520b0e1018471d67b743cd34156f498371b6
TheKitchenPan1 <153659491+TheKitchenPan1@users.noreply.github.com>
2025-01-11 18:05:26 -0500

    format

--- src/main/java/frc/robot/subsystems/Reflector/Reflector.java
+++ src/main/java/frc/robot/subsystems/Reflector/Reflector.java
@@ -1,16 +1,17 @@
-package frc.robot.subsystems.Reflector;
-
-import org.littletonrobotics.junction.Logger;
+package frc.robot.subsystems.reflector;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.Logger;
 
-public class Reflector extends SubsystemBase{
+public class Reflector extends SubsystemBase {
   private DigitalInput photoelectricSensor;
 
   public Reflector() {
-    photoelectricSensor = new DigitalInput(9); // notsure, also change number t actual dio port used.*/
+    photoelectricSensor =
+        new DigitalInput(9); // notsure, also change number t actual dio port used.*/
   }
+
   @Override
   public void periodic() {
     Logger.recordOutput("Reflector/CoralDetected", photoelectricSensor.get());
